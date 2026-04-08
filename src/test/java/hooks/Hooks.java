06f869d4b668d6f7c35fae27a087f18efb43d3dc
aryanycce <asukhdewe@gmail.com>
2026-04-08 22:05:02 +0530

    k

--- src/test/java/hooks/Hooks.java
+++ src/test/java/hooks/Hooks.java
@@ -1,43 +1,35 @@
 package hooks;
 
-import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.ExtentTest;
-
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
+
 import utils.DriverFactory;
 import utils.ExtentManager;
 
-public class Hooks 
-{
-    private static ExtentReports extent = ExtentManager.getInstance();
-    public static ThreadLocal<ExtentTest> test = new ThreadLocal<>();
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+
+public class Hooks {
+
+    static ExtentReports extent = ExtentManager.getInstance();
+    static ExtentTest test;
 
     @Before
-    public void setup(Scenario scenario) 
-    {
-        // Start the report logger for this specific scenario
-        ExtentTest extentTest = extent.createTest(scenario.getName());
-        test.set(extentTest);
-        
-        // Start the Grid Browser
+    public void setup(Scenario scenario) {
+
+        test = extent.createTest(scenario.getName());
+
         DriverFactory.initDriver();
+
     }
 
     @After
-    public void tearDown(Scenario scenario) 
-    {
-        if (scenario.isFailed()) 
-        {
-            test.get().fail("Scenario Failed!");
-        } 
-        else 
-        {
-            test.get().pass("Scenario Passed!");
-        }
+    public void tearDown() {
 
         DriverFactory.quitDriver();
-        extent.flush(); // Save the HTML report
+
+        extent.flush();   // THIS LINE CREATES REPORT FILE
+
     }
 }
--- src/test/java/utils/ExtentManager.java
+++ src/test/java/utils/ExtentManager.java
@@ -1,6 +1,6 @@
 package utils;
 
-import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.*;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
 public class ExtentManager {
@@ -11,8 +11,10 @@ public class ExtentManager {
 
         if (extent == null) {
 
+            String reportPath = System.getProperty("user.dir") + "/target/ExtentReport.html";
+
             ExtentSparkReporter reporter =
-                    new ExtentSparkReporter("target/ExtentReport.html");
+                    new ExtentSparkReporter(reportPath);
 
             extent = new ExtentReports();
 
